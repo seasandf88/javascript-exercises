0656b041a4f5850c4c69e5eca38984137326fee2
seasandf88 <engr.f.nassar@gmail.com>
2023-01-04 13:07:49 +0200

    Add another solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,10 +1,13 @@
-const reverseString = function(str) {
-    let revStr ='';
-    for (let i = str.length-1; i >=0; i--) {
-//        console.log(str[i]);
-        revStr += str[i];
-    }
-    return(revStr);
+const reverseString = function (str) {
+  let revStr;
+  // Solution 1:
+  revStr = str.split("").reverse().join("");
+  // Solution 2:
+  //     for (let i = str.length-1; i >=0; i--) {
+  // //        console.log(str[i]);
+  //         revStr += str[i];
+  //     }
+  return revStr;
 };
 
 // Do not edit below this line
