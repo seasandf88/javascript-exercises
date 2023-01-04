c43e54fb5d452711a2eb7ce927c7441603585519
seasandf88 <engr.f.nassar@gmail.com>
2023-01-04 12:25:07 +0200

    Add another solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,14 +1,17 @@
-const repeatString = function(str,num) {
-    let repStr ='';
-    if (num>0) {
-        repStr = str;
-        for (let i = 1; i < num; i++) {
-            repStr += str;
-        }
-    } else if (num<0) {
-        repStr = 'ERROR';
-    }
-    return repStr;
+const repeatString = function (str, num) {
+  let repStr = "";
+  if (num > 0) {
+    repStr = str;
+    // Solution 1:
+    repStr = str.repeat(num);
+    // Solution 2:
+    // for (let i = 1; i < num; i++) {
+    //     repStr += str;
+    // }
+  } else if (num < 0) {
+    repStr = "ERROR";
+  }
+  return repStr;
 };
 // Do not edit below this line
 module.exports = repeatString;
